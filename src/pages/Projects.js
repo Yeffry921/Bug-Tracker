61920473707ca8af569c186b345cee48428490a7
yeffry <yeffry45@gmail.com>
2022-07-25 15:42:47 -0400

    rework Project page component

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import DataTable from "../components/DataTable";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(new Date());
 
-export default Projects
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box flex={8} p={2}>
+      <Stack justifyContent="space-between" direction="row">
+        <Typography variant="h5">Projects</Typography>
+
+        <Button variant="contained" onClick={handleClickOpen}>
+          <Typography
+            variant="span"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            Add Project
+          </Typography>
+          <AddIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        </Button>
+
+        <Dialog open={open} onClose={handleClose} fullWidth>
+          <DialogTitle>Add Project</DialogTitle>
+          <DialogContent>
+            <Stack spacing={3}>
+              <TextField
+                id="standard-basic"
+                variant="outlined"
+                label="Project Name"
+                sx={{mt: '5px'}}
+              />
+
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Description"
+                maxRows={3}
+                multiline
+                type="email"
+                fullWidth
+                variant="outlined"
+              />
+
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DesktopDatePicker
+                  label="Start Date"
+                  inputFormat="MM/dd/yyyy"
+                  value={value}
+                  onChange={handleChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+
+                <DesktopDatePicker
+                  label="Due Date"
+                  inputFormat="MM/dd/yyyy"
+                  value={value}
+                  onChange={handleChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Stack>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" onClick={handleClose}>Add</Button>
+            <Button variant="outlined" onClick={handleClose}>Cancel</Button>
+          </DialogActions>
+        </Dialog>
+      </Stack>
+
+      <DataTable />
+    </Box>
+  );
+};
+
+export default Projects;
