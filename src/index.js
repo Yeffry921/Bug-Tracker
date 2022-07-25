b7cbd23607086cf8ad6661b6c732b429574cf068
yeffry <yeffry45@gmail.com>
2022-07-25 15:38:02 -0400

    remove Router from index file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Projects from "./pages/Projects";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} >
-          <Route path="/project" element={<Projects />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CssBaseline/>
+    <App/>
   </React.StrictMode>
 );
