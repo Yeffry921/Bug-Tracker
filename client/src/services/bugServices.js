d7211278133dad42c8ea28b68ddf12dbabd95418
yeffry <yeffry45@gmail.com>
2022-08-02 12:47:37 -0400

    move services to respective directory

---
+++ client/src/services/bugServices.js
@@ -0,0 +1,55 @@
+const url = `http://localhost:3001/`;
+
+// READ ALL DATA SERVICE
+
+const getAllBugData = async () => {
+  const data = await fetch("http://localhost:3001/bugs");
+  const response = await data.json();
+  return response.bugs;
+};
+
+// ADD DATA SERVICE
+
+const addBugData = async (newbug) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(newbug),
+  };
+
+  const data = await fetch("http://localhost:3001/bugs", options);
+  const response = await data.json();
+  return response;
+};
+
+// CHANGE bug STATUS SERVICE
+
+const changeBugStatus = async (id, changedStatus) => {
+  const data = await fetch(`http://localhost:3001/bugs/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(changedStatus),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+
+  const response = await data.json();
+  return response;
+};
+
+const deleteBug = async (id) => {
+  const data = await fetch(`http://localhost:3001/bugs/${id}`, {
+    method: 'DELETE',
+  })
+  const response = await data.json()
+  return response
+};
+
+export default {
+  getAllBugData,
+  addBugData,
+  changeBugStatus,
+  deleteBug,
+};
