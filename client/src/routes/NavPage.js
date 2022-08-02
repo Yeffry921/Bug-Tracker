0e88569f2a5cb96f97a3255156c50b305d9942a6
yeffry <yeffry45@gmail.com>
2022-08-02 11:36:51 -0400

    add Project and Bug Providers to NavPage

--- client/src/routes/NavPage.js
+++ client/src/routes/NavPage.js
@@ -1,14 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import BugPage from "../pages/BugPage"
-import Projects from "../pages/Projects"
+import { Route, Routes } from "react-router-dom";
+import { BugProvider } from "../bug-context";
+import BugPage from "../pages/BugPage";
+import Projects from "../pages/Projects";
+import { ProjectProvider } from "../project-context";
 
 const NavPages = () => {
   return (
-    <Routes>
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/bugs/:id" element={<BugPage />} />
-    </Routes>
-  )
-}
+    <ProjectProvider>
+      <BugProvider>
+        <Routes>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/bugs/:id" element={<BugPage />} />
+        </Routes>
+      </BugProvider>
+    </ProjectProvider>
+  );
+};
 
-export default NavPages
+export default NavPages;
