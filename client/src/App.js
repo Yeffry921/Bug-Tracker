7fe330367ccfbde85affd487d0defeef47fe037b
yeffry <yeffry45@gmail.com>
2022-08-29 13:02:44 -0400

    refactor App component with routing implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
 import BugTracker from "./components/BugTracker";
+import Dashboard from "./components/Dashboard";
+import Layout from "./components/Layout";
+import AuthContext from "./context/AuthProvider";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <BugTracker />
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="home" element={<BugTracker/>} />
+      </Route>
+    </Routes>
   );
 };
 
