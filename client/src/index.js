c250d110157d848d52cd79efc49a058936c9fc61
yeffry <yeffry45@gmail.com>
2022-08-29 13:03:06 -0400

    refactor react router index

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './index.css'
+import "./index.css";
 import { CssBaseline } from "@mui/material";
+import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CssBaseline/>
-    <App/>
+    <AuthProvider>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
