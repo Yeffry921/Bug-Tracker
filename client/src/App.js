cf1bf1935f0a1c7610f29d17d5c0e8bbf91422a8
yeffry <yeffry45@gmail.com>
2022-08-29 13:43:38 -0400

    add protected routing for App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import BugTracker from "./components/BugTracker";
 import Dashboard from "./components/Dashboard";
@@ -6,6 +5,7 @@ import Layout from "./components/Layout";
 import AuthContext from "./context/AuthProvider";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import RequireAuth from "./components/RequireAuth";
 
 const App = () => {
   return (
@@ -13,7 +13,9 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="home" element={<BugTracker/>} />
+        <Route element={<RequireAuth />}>
+          <Route path="home" element={<BugTracker />} />
+        </Route>
       </Route>
     </Routes>
   );
