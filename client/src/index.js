97402cfc22b2692e94630ee0efec92a125232402
yeffry <yeffry45@gmail.com>
2022-08-30 13:04:58 -0400

    add context providers on root index page

--- client/src/index.js
+++ client/src/index.js
@@ -5,17 +5,23 @@ import "./index.css";
 import { CssBaseline } from "@mui/material";
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ProjectProvider } from "./context/project-context";
+import { BugProvider } from "./context/bug-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
+      <ProjectProvider>
+        <BugProvider>
+          <CssBaseline />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </BrowserRouter>
+        </BugProvider>
+      </ProjectProvider>
     </AuthProvider>
   </React.StrictMode>
 );
