1e41f82ddd6a2edad33b423b52a8bf14fe33280e
yeffry <yeffry45@gmail.com>
2022-08-30 13:03:16 -0400

    fix issue with nested routes not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import BugTracker from "./components/BugTracker";
 import Dashboard from "./components/Dashboard";
-import Layout from "./components/Layout";
-import AuthContext from "./context/AuthProvider";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import RequireAuth from "./components/RequireAuth";
+import Projects from "./pages/Projects";
+import BugPage from "./pages/BugPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route element={<RequireAuth />}>
-          <Route path="home" element={<BugTracker />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<RequireAuth />}>
+        <Route path="home" element={<BugTracker />}>
+          <Route path="projects" element={<Projects />} />
+          <Route path="bugs/:id" element={<BugPage />} />
         </Route>
       </Route>
     </Routes>
