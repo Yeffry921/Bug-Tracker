28dbd55cdfe25f4999b0001a14136e6bd0055f0c
yeffry <yeffry45@gmail.com>
2022-08-30 13:17:55 -0400

    fix issue with Bug model missing from server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const url = `mongodb+srv://yeffry921:Rs8XfzjK1exwSul4@bug-cluster.elbafr3.mongodb.net/?retryWrites=true&w=majority`;
-const Project = require('./models/project')
+const Project = require("./models/project");
+const Bug = require("./models/bug");
 
 app.use(cors());
 app.use(express.json());
@@ -12,8 +13,6 @@ const connectionParams = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-
-
 
 mongoose
   .connect(url, connectionParams)
@@ -29,7 +28,7 @@ app.get("/projects", async (req, res) => {
   res.json({ projects });
 });
 
-// // CREATE PROJECT
+// CREATE PROJECT
 
 app.post("/projects", (req, res) => {
   // post project into project DB
@@ -68,13 +67,12 @@ app.delete("/projects/:id", async (req, res) => {
   });
 });
 
-
 // GET ALL BUGS CORRESPONDING TO PROJECT CLICKED
 
 app.get("/bugs/:id", async (req, res) => {
   const id = req.params.id;
-  const data = await Bug.find({ related_project_id: { $in: id } })
-  res.json(data)
+  const data = await Bug.find({ related_project_id: { $in: id } });
+  res.json(data);
 });
 
 // // CREATE A NEW BUG AND SAVE TO DB
@@ -101,20 +99,20 @@ app.post("/bugs", (req, res) => {
 
 // UPDATE BUG STATUS
 
-app.put('/bugs/:id', (req, res) => {
-  const id = req.params.id
+app.put("/bugs/:id", (req, res) => {
+  const id = req.params.id;
   const newStatus = req.body.status;
 
-  Bug.findByIdAndUpdate(id, { status: newStatus }, { new: true }).then(
-    (data) => res.json(data)
+  Bug.findByIdAndUpdate(id, { status: newStatus }, { new: true }).then((data) =>
+    res.json(data)
   );
-})
+});
 
 // DELETE BUGS
 
 app.delete("/bugs/:id", async (req, res) => {
   const id = req.params.id;
-  
+
   Bug.findByIdAndDelete(id).then((data) => {
     res.status(200).json({ message: "Bug Deleted" });
   });
