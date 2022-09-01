5cf913a42a21e36a2f4d1f5d85bc50c7b952dbb4
yeffry <yeffry45@gmail.com>
2022-09-01 17:24:45 -0400

    testing style changes

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -11,7 +11,7 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="#333" flex={1} sx={{ padding: "10px 0", height: '100%', flex: 1 }}>
+    <Box bgcolor="#444" sx={{ flexBasis: "0 0 10%" }}>
       <List>
         {mainNavbarItems.map((item, index) => (
           <NavLink to={item.route} style={{textDecoration: 'none'}} key={item.id}>
